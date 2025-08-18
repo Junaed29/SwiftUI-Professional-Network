973d9e17be8dd5d9e904f8a4a6b02a6f244f7f64
Junaed <junaed1995@gmail.com>
2025-08-18 09:51:01 +0800

    Initial Commit

---
+++ ProfessionalNetworkingApp/ProfessionalNetworkingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProfessionalNetworkingAppApp.swift
+//  ProfessionalNetworkingApp
+//
+//  Created by Junaed Chowdhury on 18/8/25.
+//
+
+import SwiftUI
+
+@main
+struct ProfessionalNetworkingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
